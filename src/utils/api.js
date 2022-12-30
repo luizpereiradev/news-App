95d65a4752614c7937652a15c8aec4aae2cca661
Luiz Felipe <84043551+luizpereiradev@users.noreply.github.com>
2022-12-30 01:55:21 -0300

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 export const API_URL = 'https://api.bing.microsoft.com/v7.0/news/search'
-export const API_KEY = process.env.API_KEY
-export const CLIENT_ID = process.env.CLIENT_ID
+export const API_KEY = 'a0ff9b6bd16d49b6b884ba423c4c6541'
+export const CLIENT_ID = '54666b4e-e74d-48ef-8ab2-ad5e15e3b379'
 
 export function GET_TOP(page, q,count = 10) {
   return {
